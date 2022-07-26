26f6de3df41b6f1754414f5acfbb0b36b48b1882
Naranja <naranjaspintadas@gmail.com>
2022-07-26 19:46:42 -0300

    ejemplo clases 7

--- clase_7_Intro_JS/js/main.js
+++ clase_7_Intro_JS/js/main.js
@@ -9,7 +9,7 @@ Comentario multilínea
 
 */
 
-
+/*
 console.log("Hola mundo");
 
 // Variable
@@ -36,3 +36,50 @@ console.log (resutadoSuma);
 
 let resultadoResta = numeroB - numeroA;
 console.log (resultadoResta);
+
+// Multiplicar
+
+let resultadoProducto = numeroB * NUMEROC;
+console.log(resultadoProducto);
+
+
+// Concatenar Strings
+
+
+let textoA = "FULL";
+let textoB = "Stack";
+
+const BLANCO = " ";
+
+console.log (textoA + textoB);
+console.log(textoB + 1);
+console.log (textoA + BLANCO + textoB)
+
+// PROMPT
+
+let nombreIngresado = prompt ("Ingresá tu nombre");
+alert ("Bienvenido"+" " + nombreIngresado);
+
+let numeroA = parseInt (prompt ("Ingresá un número"));
+
+const NUMEROC = 30; 
+
+let salida = numeroA + NUMEROC;
+
+alert (salida);*/
+
+// Arrays
+
+let datos = ['dato1', 'dato2', 'dato3', 'dato4', 'dato5' ];
+
+console.log (datos[0]);
+
+let colorMascotas = [
+    ['perro', 'marron', 'gris', 'rojo'],
+    ['gato', 'blanco', 'negro'],
+    ['pez', 'naranja'],
+    ['jiraja', 'amarilla']
+]
+
+console.log(colorMascotas[1][2]);
+console.log(colorMascotas[1][1].length)
