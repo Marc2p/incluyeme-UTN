3657dbfba70e3804f44486a4abb2155e984b4696
Naranja <naranjaspintadas@gmail.com>
2022-07-26 18:44:22 -0300

    ejemplo clases 7

---
+++ clase_7_Intro_JS/js/main.js
@@ -0,0 +1,38 @@
+/** 
+ * Clase 7: Introducción a JS
+ *  */ 
+
+// Comentario de una línea
+
+/* Comentario multilinea
+Comentario multilínea
+
+*/
+
+
+console.log("Hola mundo");
+
+// Variable
+
+let variableNumerica;
+let variableTexto;
+
+
+variableNumerica = 5;
+variableTexto = "Mi texto";
+
+console.log(variableNumerica, variableTexto);
+
+let numeroA = 1;
+let numeroB = 2;
+const NUMEROC = 3;
+
+// Sumar 
+console.log(numeroA + numeroB);
+let resutadoSuma = numeroA + numeroB;
+console.log (resutadoSuma);
+
+// Restar 
+
+let resultadoResta = numeroB - numeroA;
+console.log (resultadoResta);
